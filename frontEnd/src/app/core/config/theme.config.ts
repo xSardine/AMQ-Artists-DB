b53ef20ec97c39311915dd5b3fbf48fea1eb8e46
xSardine <79758127+xSardine@users.noreply.github.com>
2024-11-05 11:49:59 +0100

    code formatter pass 2

--- frontEnd/src/app/core/config/theme.config.ts
+++ frontEnd/src/app/core/config/theme.config.ts
@@ -1,29 +1,30 @@
 export const THEMES = {
-  default: {
-    primaryColor: '#222',
-    secondaryColor: '#1976d2',
-    songTableHoverColor: '#222',
-    songTableImpairColor: '#333',
-    songTablePairColor: '#555',
-    tableFontColor: 'white',
-    textOnPrimary: 'white',
-    textOnSecondary: 'black',
-    background: '#555',
-    textOnBackground: 'white',
-    linkOnBackground: 'white',
-  },
 
-  dark: {
-    primaryColor: '#1976d2',
-    secondaryColor: '#666',
-    songTableHoverColor: '#ddd',
-    songTableImpairColor: '#f2f2f2',
-    songTablePairColor: 'white',
-    textOnPrimary: 'white',
-    tableFontColor: 'black',
-    textOnSecondary: 'black',
-    background: 'white',
-    textOnBackground: 'black',
-    linkOnBackground: 'blue',
-  },
+    default: {
+        primaryColor: "#222",
+        secondaryColor: "#1976d2",
+        songTableHoverColor: "#222",
+        songTableImpairColor: "#333",
+        songTablePairColor: "#555",
+        tableFontColor: "white",
+        textOnPrimary: "white",
+        textOnSecondary: "black",
+        background: "#555",
+        textOnBackground: "white",
+        linkOnBackground: "white"
+    },
+
+    dark: {
+        primaryColor: "#1976d2",
+        secondaryColor: "#666",
+        songTableHoverColor: "#ddd",
+        songTableImpairColor: "#f2f2f2",
+        songTablePairColor: "white",
+        textOnPrimary: "white",
+        tableFontColor: "black",
+        textOnSecondary: "black",
+        background: "white",
+        textOnBackground: "black",
+        linkOnBackground: "blue"
+    }
 };
--- frontEnd/src/app/core/services/theme.service.ts
+++ frontEnd/src/app/core/services/theme.service.ts
@@ -1,24 +1,25 @@
-import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable } from '@angular/core';
-import { THEMES } from '../config/theme.config';
+import { DOCUMENT } from "@angular/common";
+import { Inject, Injectable } from "@angular/core";
+import { THEMES } from "../config/theme.config";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: "root"
 })
 export class ThemeService {
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+    constructor(@Inject(DOCUMENT) private document: Document) { }
 
-  current_theme: string = 'default';
-  toggleTheme() {
-    if (this.current_theme == 'default') {
-      this.current_theme = 'dark';
-    } else {
-      this.current_theme = 'default';
+    current_theme: string = "default"
+    toggleTheme() {
+        if (this.current_theme == "default") {
+            this.current_theme = "dark";
+        }
+        else {
+            this.current_theme = "default";
+        }
+        // @ts-ignore
+        const theme = THEMES[this.current_theme];
+        Object.keys(theme).forEach((key) => {
+            this.document.documentElement.style.setProperty(`--${key}`, theme[key]);
+        });
     }
-    // @ts-ignore
-    const theme = THEMES[this.current_theme];
-    Object.keys(theme).forEach((key) => {
-      this.document.documentElement.style.setProperty(`--${key}`, theme[key]);
-    });
-  }
 }
